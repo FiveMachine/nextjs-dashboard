57d60d1c90b2087827835f01353e437ed34c9ddd
FiveMachine <tlee07299@gmail.com>
2024-03-20 05:37:50 -0400

    fix:

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en">   
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
